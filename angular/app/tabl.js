476250cf14baa2e621595f033e4c392b6efeec93
Tawhida Shorif <tawhida016@gmail.com>
2019-09-17 17:24:51 +0600

    angularJs

---
+++ angular/app/tabl.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+	$scope.items =[
+		{name:"tawhida",phone : "56653"},
+		{name:"mim",phone : "54653"},
+		{name:"shorif",phone : "674554"},
+		{name:"tawhida",phone : "56653"}
+	];
+	
+});							  				  						   
+								   
